f8ef88f2750e50ab73831b735aababfbd1bceacc
Jason Sim <jason.young.sim@gmail.com>
2024-04-30 15:46:50 -0500

    u

--- server.py
+++ server.py
@@ -15,25 +15,27 @@ app = Flask(__name__)
 def get_config():
     pods = []
 
-    # your code here
+    pods = v1.list_pod_for_all_namespaces()
 
     output = {"pods": pods}
     output = json.dumps(output)
 
-    return output
+    return output, 200
 
 @app.route('/img-classification/free',methods=['POST'])
 def post_free():
-    # your code here
 
-    return "success"
-
+    f= open("free-job.yaml")
+    job_yml = yaml.safe_load(f)
+    v1.create_namespaced_job(job_yml)
+    return "success", 200
 
 @app.route('/img-classification/premium', methods=['POST'])
 def post_premium():
-    # your code here
-
-    return "success"
+    f= open("job-premium.yaml")
+    job_yml = yaml.safe_load(f)
+    v1.create_namespaced_job(job_yml)
+    return "success", 200
 
     
 if __name__ == "__main__":
